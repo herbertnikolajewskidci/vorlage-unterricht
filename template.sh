431f9bbd6ed7360f2336c80fbe51a6af6a333818
Herbert Nikolajewski <114996264+herbertnikolajewskidci@users.noreply.github.com>
2023-06-07 16:30:55 +0200

    Create template.sh

---
+++ template.sh
@@ -0,0 +1,33 @@
+new_text=$(basename `git rev-parse --show-toplevel`)
+sed -i "s/\[Datum\]\-\[Thema\]/$new_text/g" README.md
+sed -i "s/\[repo\-name\]/$new_text/g" README.md
+
+git add .
+git commit -m "updated README.md"
+git push
+
+git checkout docs
+
+sed -i "s/\[Datum\]\-\[Thema\]/$new_text/g" users.mongo.js
+
+git add .
+git commit -m "updated README.md"
+git push
+
+git checkout setup
+
+sed -i "s/\[Datum\]\-\[Thema\]/$new_text/g" users.mongo.js
+
+git add .
+git commit -m "updated README.md"
+git push
+
+git checkout final
+
+sed -i "s/\[Datum\]\-\[Thema\]/$new_text/g" users.mongo.js
+
+git add .
+git commit -m "updated README.md"
+git push
+
+git checkout main
